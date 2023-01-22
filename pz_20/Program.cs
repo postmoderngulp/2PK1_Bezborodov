234e1e9be994764ad4a00166d0b1e2d6df75ebd5
polichinel <bezborodovstanislav109@gmail.com>
2023-01-22 22:40:08 +0300

    test

---
+++ pz_20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PZ_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+            StringRead(text);
+        }
+
+        static void StringRead(string text)
+        {
+            string result = "";
+            try
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+
+                    if (Char.IsUpper(text[i]))
+                        result += text[i].ToString().ToLower();
+                    else
+                        result += text[i].ToString().ToUpper();
+
+                }
+                Console.WriteLine(result);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+            
+        }
+    }
+}
