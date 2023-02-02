6d8f5aadd6ccfd2b1c1d8f1753f1d4feafd3d408
polichinel <bezborodovstanislav109@gmail.com>
2023-02-02 11:06:24 +0300

    test

---
+++ PZ_21/Computer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_21
+{
+    class Computer
+    {
+        int ID;
+        double totalHDDMemory;
+        double processorFrequency;
+
+        public Computer()
+        {
+
+        }
+        public Computer(int id,double totalMemory,double procFrequency)
+        {
+            ID = id;
+            totalHDDMemory = totalMemory;
+            processorFrequency = procFrequency;
+        }
+        public Computer(int id, double totalMemory)
+        {
+            ID = id;
+            totalHDDMemory = totalMemory;
+        }
+        public Computer(double totalMemory, double procFrequency)
+        {
+            totalHDDMemory = totalMemory;
+            processorFrequency = procFrequency;
+        }
+
+        public void GetCompName()
+        {
+            Console.WriteLine($"ID:{ID}");
+            Console.WriteLine($"totalHDDMemory:{totalHDDMemory}Gb");
+            Console.WriteLine($"processorFrequency:{processorFrequency}Ghz");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PZ_21/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PZ_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer pc1 = new Computer();
+            Computer pc2 = new Computer(8800,512,3.2);
+            Computer pc3 = new Computer(2900,256);
+            Computer pc4 = new Computer( totalMemory :1024,4.4);
+
+            pc1.GetCompName();
+            pc2.GetCompName();
+            pc3.GetCompName();
+            pc4.GetCompName();
+        }
+    }
+}
